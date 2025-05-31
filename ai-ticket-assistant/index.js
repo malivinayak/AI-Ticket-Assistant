310f06a2a6e3794a30b95058f562207bdf7b731c
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2025-05-31 08:24:56 +0530

    Backend setup

---
+++ ai-ticket-assistant/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected✅");
+        app.listen(PORT, () => console.log("🚀 Server at http://localhost:3000"));
+    })
+    .catch((err) => console.error("❌ MongoDB error: ", err))
